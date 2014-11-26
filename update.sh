cb0fb3f8fcecbabfd157e59b8646f92b8d670492
uroboro <uroboro845@gmail.com>
2014-11-26 00:29:20 -0300

    Updated Makefile and update.sh; added WOLListener

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
-cd repo; make clean all; cd ..
+main() {
+	argc=$#
+	argv=($@)
+	for (( i = 0; i < $argc; i++ )); do
+		echo "argv[$i] = ${argv[$i]}"
+	done
 
-#git add repo;
+	# Build Release, Packages and Packages.bz2
+	cd repo
+	make
+	cd ..
+
+	#git add repo;
+
+}
+
+main $0 $@
