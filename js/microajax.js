03e5e60c03e744ac2c3c14b237544119a174a6bf
Oliver Boudet <oboudet1@me.com>
2018-10-18 21:13:33 +0200

    revert microajax, as it is a 3rd party dependency

--- js/microajax.js
+++ js/microajax.js
@@ -20,43 +20,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-function microAjax(url, callbackFunction) {
-	this.bindFunction = function(caller, object) {
+function microAjax(url, callbackFunction)
+{
+	this.bindFunction = function (caller, object) {
 		return function() {
 			return caller.apply(object, [object]);
 		};
 	};
 
-	this.stateChange = function(object) {
-		if (this.request.readyState == 4)
+	this.stateChange = function (object) {
+		if (this.request.readyState==4)
 			this.callbackFunction(this.request.responseText);
 	};
 
 	this.getRequest = function() {
-		if (window.ActiveXObject) return new ActiveXObject('Microsoft.XMLHTTP');
-		else if (window.XMLHttpRequest) return new XMLHttpRequest();
+		if (window.ActiveXObject)
+			return new ActiveXObject('Microsoft.XMLHTTP');
+		else if (window.XMLHttpRequest)
+			return new XMLHttpRequest();
 		return false;
 	};
 
-	this.postBody = arguments[2] || '';
+	this.postBody = (arguments[2] || "");
 
-	this.callbackFunction = callbackFunction;
-	this.url = url;
+	this.callbackFunction=callbackFunction;
+	this.url=url;
 	this.request = this.getRequest();
-
-	if (this.request) {
+	
+	if(this.request) {
 		var req = this.request;
 		req.onreadystatechange = this.bindFunction(this.stateChange, this);
 
-		if (this.postBody !== '') {
-			req.open('POST', url, true);
+		if (this.postBody!=="") {
+			req.open("POST", url, true);
 			req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 			req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 			req.setRequestHeader('Connection', 'close');
 		} else {
-			req.open('GET', url, true);
+			req.open("GET", url, true);
 		}
 
 		req.send(this.postBody);
 	}
 }
+
