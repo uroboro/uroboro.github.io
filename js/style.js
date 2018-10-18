5d3a398a1f83ef1955a565f01c60fe8601663a08
Oliver Boudet <oboudet1@me.com>
2018-10-18 21:23:16 +0200

    unindent

--- js/style.js
+++ js/style.js
@@ -8,12 +8,12 @@ function styleBasicButtonStyle() {
 	style += 'border-radius: 5px;';
 	style += '-webkit-border-radius: 5px;';
 	style += '-moz-border-radius: 5px;';
-	/*
+/*
 	style += "border-top: 1px solid #cbcbcb;";
 	style += "box-shadow: rgba(0,0,0,1) 0 1px 0;";
 	style += "-webkit-box-shadow: rgba(0,0,0,1) 0 1px 0;";
 	style += "-moz-box-shadow: rgba(0,0,0,1) 0 1px 0;";
-	*/
+*/
 	// TEXT
 	style += 'vertical-align: middle;';
 	style += 'color: white;';
@@ -23,13 +23,13 @@ function styleBasicButtonStyle() {
 
 	// BACKGROUND
 	style += 'background: #a4a4a4;';
-	/*
+/*
 	background: -webkit-gradient(linear, left top, left bottom, from(#737373), to(#a4a4a4));
 	background: -webkit-linear-gradient(top, #737373, #a4a4a4);
 	background: -moz-linear-gradient(top, #737373, #a4a4a4);
 	background: -ms-linear-gradient(top, #737373, #a4a4a4);
 	background: -o-linear-gradient(top, #737373, #a4a4a4);
-	*/
+*/
 	return style;
 }
 
